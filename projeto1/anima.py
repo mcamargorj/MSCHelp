ab39641429cdd373d176e2bc13589773ac7c80a2
mcamargorj <mcamargorj@gmail.com>
2024-04-21 13:19:56 -0300

    atuallizações

--- projeto1/anima.py
+++ projeto1/anima.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
 
 # Função para atualizar a animação
 def update(frame):
