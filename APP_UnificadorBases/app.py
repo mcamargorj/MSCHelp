abe4c794f9d6350cdd7f02a174d74c6df606ad8c
mcamargorj <mcamargorj@gmail.com>
2024-05-27 15:24:54 -0300

    ajustes no projeto APP_UnificadorBases

--- APP_UnificadorBases/app.py
+++ APP_UnificadorBases/app.py
@@ -5,7 +5,7 @@ import openpyxl
 import xlrd
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta'
+app.secret_key = os.urandom(24)
 
 DIRETORIO_UPLOADS = 'uploads'
 EXTENSOES_PERMITIDAS = {'csv', 'xlsx', 'xls'}
